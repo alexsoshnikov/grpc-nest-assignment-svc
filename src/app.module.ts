46f1f221988a33777551d1830a31e29814768dbd
alexsoshnikov <alex.soshnikov@mail.ru>
2022-05-11 02:26:13 +0400

    Initial microservice

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'micro_order',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    OrderModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
